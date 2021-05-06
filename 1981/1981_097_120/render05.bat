7d3c23a193eb4a5a00ee2404d4d4d3f3486f6904
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-05-06 19:45:20 +0200

    1981 - 99 - 100

---
+++ 1981/1981_097_120/render05.bat
@@ -0,0 +1,4 @@
+#"D:\Program Files\VirtualDub\vdub64.exe" /s v97.vdscript
+#"D:\Program Files\VirtualDub\vdub64.exe" /s v98.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v99.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v100.vdscript
