82f99b17be0cbb8f1dca2585db3fd5f447c8301d
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-05-12 13:12:27 +0200

    1981 - 169 - 192

---
+++ 1981/1981_169_192/render08.bat
@@ -0,0 +1,10 @@
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v169.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v170.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v171.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v172.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v173.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v174.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v175.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v176.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v177.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v178.vdscript
