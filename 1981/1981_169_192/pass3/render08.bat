abffe99942514dc61355f4a8838102678607f4b6
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-05-17 16:42:30 +0200

    1981 - 179 - 192

--- 1981/1981_169_192/pass3/render08.bat
+++ 1981/1981_169_192/pass3/render08.bat
@@ -1,10 +1,24 @@
-"D:\Program Files\VirtualDub\vdub64.exe" /s v169.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v170.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v171.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v172.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v173.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v174.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v175.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v176.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v177.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v178.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v169.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v170.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v171.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v172.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v173.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v174.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v175.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v176.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v177.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v178.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v179.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v180.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v181.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v182.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v183.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v184.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v185.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v186.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v187.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v188.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v189.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v190.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v191.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v192.vdscript
--- 1981/1981_169_192/render08.bat
+++ 1981/1981_169_192/render08.bat
@@ -7,4 +7,17 @@ rem "D:\Program Files\VirtualDub\vdub64.exe" /s v174.vdscript
 rem "D:\Program Files\VirtualDub\vdub64.exe" /s v175.vdscript
 rem "D:\Program Files\VirtualDub\vdub64.exe" /s v176.vdscript
 rem "D:\Program Files\VirtualDub\vdub64.exe" /s v177.vdscript
-"D:\Program Files\VirtualDub\vdub64.exe" /s v178.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v178.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v179.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v181.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v182.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v183.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v184.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v185.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v186.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v187.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v188.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v189.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v190.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v191.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v192.vdscript
