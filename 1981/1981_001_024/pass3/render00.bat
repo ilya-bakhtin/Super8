bea14dc90daa40379503edeef47caebff62d54af
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-05-20 19:19:15 +0200

    1981 - 001 - 024

---
+++ 1981/1981_001_024/pass3/render00.bat
@@ -0,0 +1,24 @@
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v1.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v2.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v3.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v4.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v5.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v6.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v7.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v8.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v9.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v10.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v11.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v12.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v13.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v14.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v15.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v16.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v17.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v18.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v19.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v20.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v21.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v22.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v23.vdscript
+rem "D:\Program Files\VirtualDub\vdub64.exe" /s v24.vdscript
