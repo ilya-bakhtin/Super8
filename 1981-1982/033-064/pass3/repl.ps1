8054efe810f4c9174be16dbbd7f6417a425f92e7
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-07-23 16:33:34 +0200

    1981-1982 033-064

---
+++ 1981-1982/033-064/pass3/repl.ps1
@@ -0,0 +1,4 @@
+Get-ChildItem '.\*.avs' -Recurse | ForEach {
+(Get-Content $_ | ForEach { $_ -replace '11449 Bakhtin 02 N8.720p_04.mov.avs', '..\11943 Bakhtin N8 01.720p_01-33.mov.avs' }) |
+Set-Content $_
+}
