84b5c849c049da40fd9868df912b5e9cae30b63e
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2022-07-08 08:39:15 +0200

    1982-skola - initial upload

---
+++ 1982-skola/001-032/r26.bat
@@ -0,0 +1,6 @@
+"D:\Program Files\VirtualDub\vdub64.exe" /s v26.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v27.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v28.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v29.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v30.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s v31.vdscript
