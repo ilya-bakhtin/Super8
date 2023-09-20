c6e866bba86eab1c7e66f845cc4c602df61c73d1
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-09-20 13:17:51 +0200

    1983/Moskva-Tallinn/065-096

---
+++ 1983/Moskva-Tallinn/065-096/deshake/render.bat
@@ -0,0 +1,64 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v65.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v66.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v67.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v68.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v69.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v70.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v71.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v72.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v73.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v74.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v75.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v76.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v77.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v78.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v79.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v80.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v81.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v82.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v83.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v84.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v85.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v86.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v87.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v88.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v89.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v90.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v91.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v92.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v93.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v94.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v95.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\065-096\deshake\v96.vdscript
+
