89e3aa06d57c0b1d24ebcaa46eaff6596d857ab4
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-09-20 13:31:53 +0200

    1983/Moskva-Tallinn - common files update

--- 1983/Moskva-Tallinn/combined/render-step.bat
+++ 1983/Moskva-Tallinn/combined/render-step.bat
@@ -4,7 +4,7 @@ SET dir_f=%dir:m:\=e:\%
 SET dir_f=%dir_f:video\=video\tmp\%
 SET dir_f_ds=%dir_f:\=\\%
 
-    "cmd.exe" /c ""C:\Users\ilx\wrk\ffmpeg\ffmpeg.exe"             -loglevel level+error -i "%2.avs" -strict -1 -f yuv4mpegpipe - | "C:\Users\ilx\wrk\x265\x265.exe"                    --crf 12 --preset=slow --tune grain --sar 1:1 -o "%1\%2.h265" --y4m -"
+    "cmd.exe" /c ""C:\Users\ilx\wrk\ffmpeg\ffmpeg.exe"             -loglevel level+error -i "%2.avs" -strict -1 -f yuv4mpegpipe - | "C:\Users\ilx\wrk\x265\x265.exe"                    --crf 17 --preset=slow --tune grain --sar 1:1 -o "%1\%2.h265" --y4m -"
 rem "cmd.exe" /c ""D:\Program Files\MeGUI\tools\ffmpeg\ffmpeg.exe" -loglevel level+error -i "%2.avs" -strict -1 -f yuv4mpegpipe - | "D:\Program Files\MeGUI\tools\x265\vs2019\x265.exe" --crf 21 --preset=slow --tune grain --sar 1:1 -o "%1\%2.h265" --y4m -"
 rem "D:\Program Files\MeGUI\tools\ffmpeg\ffmpeg.exe" -hwaccel qsv -i "%2.avs" -c:v hevc_qsv -preset veryslow -global_quality 6 -y "%1\%2.h265"
 
