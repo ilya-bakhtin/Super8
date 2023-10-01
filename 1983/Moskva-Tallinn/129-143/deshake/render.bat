303cd7cf2ffacb491db7e3ed34e9026bfc2c6dfd
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-10-01 09:18:08 +0200

    1983/Moskva-Tallinn/129-143

---
+++ 1983/Moskva-Tallinn/129-143/deshake/render.bat
@@ -0,0 +1,30 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v129.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v130.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v131.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v132.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v133.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v134.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v135.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v136.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v137.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v138.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v139.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v140.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v141.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v142.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\129-143\deshake\v143.vdscript
+
