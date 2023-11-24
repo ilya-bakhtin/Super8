6dd95fea6c02937215c01505a898e8d4326a50ea
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-11-24 18:27:20 +0100

    1983-Moskva-Tallinn - rotate related updates. combined.avs

---
+++ 1983/Moskva-Tallinn/033-064/render_one_clip.bat
@@ -0,0 +1,10 @@
+cd delogo
+call render-step.bat m:\media\video\super8\wrk\1983\Moskva-Tallinn v%1
+cd ..\deshake
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Moskva-Tallinn\033-064\deshake\v%1.vdscript
+call render-step.bat m:\media\video\super8\wrk\1983\Moskva-Tallinn v%1
+cd ..\color
+call render-step.bat m:\media\video\super8\wrk\1983\Moskva-Tallinn v%1
+cd ..\sharp
+call render-step.bat m:\media\video\super8\wrk\1983\Moskva-Tallinn v%1
+cd ..
