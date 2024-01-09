6b1300377707dc5f4f605d92817c1e5c82e8fe2c
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2024-01-09 16:42:35 +0100

    1983/Petrohrad-Kemerovo

---
+++ 1983/Petrohrad-Kemerovo/097-125/deshake/render.bat
@@ -0,0 +1,58 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v97.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v98.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v99.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v100.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v101.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v102.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v103.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v104.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v105.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v106.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v107.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v108.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v109.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v110.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v111.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v112.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v113.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v114.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v115.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v116.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v117.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v118.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v119.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v120.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v121.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v122.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v123.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v124.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1983\Petrohrad-Kemerovo\097-125\deshake\v125.vdscript
+
