c7870e7f187757a5105ff8267d78b7b9bc0e60b6
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2026-08-25 12:59:44 +0200

    1977-1979/353-384

---
+++ 1977-1979/353-384/deshake/render.bat
@@ -0,0 +1,64 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v353.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v354.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v355.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v356.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v357.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v358.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v359.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v360.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v361.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v362.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v363.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v364.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v365.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v366.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v367.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v368.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v369.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v370.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v371.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v372.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v373.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v374.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v375.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v376.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v377.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v378.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v379.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v380.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v381.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v382.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v383.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\Super8\1977-1979\353-384\deshake\v384.vdscript
+
